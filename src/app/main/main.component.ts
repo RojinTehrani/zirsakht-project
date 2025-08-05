7e42bad3aa0cddde8583db1a3f7ed4c426c47d0a
MohammadAmin Nasirvand <aminnam2845@gmail.com>
2025-08-05 16:32:47 +0330

    amin - change main layout

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { SidebarMenuComponent } from './sidebar-menu-component/sidebar-menu-component';
+// import { SidebarMenuComponent } from './sidebar-menu-component/sidebar-menu-component';
 
 
 @Component({
   selector: 'app-main',
-  imports: [SidebarMenuComponent],
+  imports: [],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
