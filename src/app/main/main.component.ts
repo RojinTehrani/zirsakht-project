10c92462a4f3b837d09ebd65d928b63b71715e8c
Rojin Tehrani <roji.tehrani@gmail.com>
2025-08-06 17:34:17 +0330

    tehrani - responsive sidebar pc screen

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { SidebarMenuComponent } from './sidebar-menu-component/sidebar-menu-component';
 import { DashboardComponent } from '../dashboard/dashboard.component';
-
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-main',
-  imports: [SidebarMenuComponent , DashboardComponent],
+  imports: [SidebarMenuComponent , DashboardComponent,CommonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
+sidebarIsCollapsed = false;
+
+
+  constructor() {
+    this.sidebarIsCollapsed = window.innerWidth < 1550;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    const width = (event.target as Window).innerWidth;
+
+    if (width < 1550 && !this.sidebarIsCollapsed) {
+      this.sidebarIsCollapsed = true;
+    }
+
+    if (width >= 1550 && this.sidebarIsCollapsed) {
+      this.sidebarIsCollapsed = false;
+    }
+  }
+  onSidebarToggle(isCollapsed: boolean) {
+    this.sidebarIsCollapsed = isCollapsed;
+    }
+
 
 }
--- src/app/main/sidebar-menu-component/sidebar-menu-component.ts
+++ src/app/main/sidebar-menu-component/sidebar-menu-component.ts
@@ -1,31 +1,47 @@
-import { AfterViewInit, Component, ElementRef, Renderer2 } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Renderer2,Output,EventEmitter,Input,SimpleChanges,OnChanges  } from '@angular/core';
 @Component({
   selector: 'app-sidebar-menu-component',
   templateUrl: './sidebar-menu-component.html',
   styleUrls: ['./sidebar-menu-component.css']
 })
-export class SidebarMenuComponent implements AfterViewInit {
+export class SidebarMenuComponent  implements AfterViewInit, OnChanges {
 
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
-  ngAfterViewInit(): void {
+@Output() collapsedChanged = new EventEmitter<boolean>();
+@Input() collapsedFromParent: boolean = false;
+sidebarLockedCollapsed:boolean = false;
+isCollapsed: boolean = false;
 
+//constructor
+constructor(private el: ElementRef, private renderer: Renderer2) {}
 
-    const dropdownToggles = this.el.nativeElement.querySelectorAll('.dropdown-toggle');
+//on changes (listening to parent for changing window width and make changes on sidebar)
+ngOnChanges(changes: SimpleChanges): void {
+  if (changes['collapsedFromParent']) {
+    this.isCollapsed = changes['collapsedFromParent'].currentValue;
+    this.sidebarLockedCollapsed = window.innerWidth <= 1550;  
+    this.updateSidebarClass();
+  }
+}
 
-    dropdownToggles.forEach((toggle: Element) => {
-      this.renderer.listen(toggle, 'click', (event: Event) => {
-        event.preventDefault(); // جلوگیری از رفتن به href="#"
+//after view init for toggle sidebar and dropdown
+ngAfterViewInit(): void {
+  this.sidebarLockedCollapsed = window.innerWidth <= 1550;
 
-        const dropdownContainer = toggle.closest('.dropdown-container');
-        const dropdownMenu = dropdownContainer?.querySelector('.dropdown-menu');
+  const dropdownToggles = this.el.nativeElement.querySelectorAll('.dropdown-toggle');
 
-        if (dropdownMenu) {
-          const isShown = dropdownMenu.classList.contains('show');
-          if (isShown) {
-            this.renderer.removeClass(dropdownMenu, 'show');
+  dropdownToggles.forEach((toggle: Element) => {
+    this.renderer.listen(toggle, 'click', (event: Event) => {
+    event.preventDefault();
+
+    const dropdownContainer = toggle.closest('.dropdown-container');
+    const dropdownMenu = dropdownContainer?.querySelector('.dropdown-menu');
+
+    if (dropdownMenu) {
+      const isShown = dropdownMenu.classList.contains('show');
+        if (isShown) {
+          this.renderer.removeClass(dropdownMenu, 'show');
           } else {
-            this.renderer.addClass(dropdownMenu, 'show');
-            
+          this.renderer.addClass(dropdownMenu, 'show');
           }
         }
       });
@@ -67,22 +83,57 @@ export class SidebarMenuComponent implements AfterViewInit {
       });
     });
 
-    // رویداد کلیک روی دکمه‌های sidebar toggle
-    document.querySelectorAll<HTMLElement>('.sidebar-toggler, .sidebar-menu-button').forEach(button => {
-      this.renderer.listen(button, 'click', () => {
-        closeAllDropdowns();
-        const sidebar = document.querySelector<HTMLElement>('.sidebar');
-        if (sidebar) sidebar.classList.toggle('collapsed');
-      });
-    });
 
-    if (window.innerWidth <= 1024) {
-      const sidebar = document.querySelector<HTMLElement>('.sidebar');
-      if (sidebar) this.renderer.addClass(sidebar, 'collapsed');
-    }
+
+ if (window.innerWidth <= 1550) {
+  this.sidebarLockedCollapsed = true; 
+  this.isCollapsed = true;
+  this.updateSidebarClass();
+  this.collapsedChanged.emit(this.isCollapsed);
+} else {
+  this.sidebarLockedCollapsed = false; 
+}
+
+
+
+    this.handleInitialCollapseOnSmallScreens();
   }
 
 
 
-  
+private updateSidebarClass(): void {
+  const sidebar = this.el.nativeElement.querySelector('.sidebar');
+  if (sidebar) {
+    if (this.isCollapsed) {
+      this.renderer.addClass(sidebar, 'collapsed');
+    } else {
+      this.renderer.removeClass(sidebar, 'collapsed');
+    }
+  }
+}
+
+  private handleInitialCollapseOnSmallScreens(): void {
+  if (window.innerWidth <= 1550) {
+    this.isCollapsed = true;
+    this.collapsedChanged.emit(this.isCollapsed);
+    this.updateSidebarClass();
+  }
+}
+
+// toggle sidebar button method
+  toggleSidebar(): void {
+  if (this.sidebarLockedCollapsed) return; 
+
+  this.isCollapsed = !this.isCollapsed;
+  this.collapsedChanged.emit(this.isCollapsed);
+  this.updateSidebarClass();
+}
+
+
+
+
+
+
+
+
 }
